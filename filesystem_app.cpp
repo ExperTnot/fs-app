b984585042406cf89b60bda6d9ddc36b1475f50c
expertnot <notexpertnot@gmail.com>
2024-08-04 16:08:08 +0200

    main fix

--- filesystem_app.cpp
+++ filesystem_app.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
-#include <fstream>
 #include <filesystem>
 #include "CommandLineParser.hpp"
+#include "FileOutput.hpp"
+#include "FileManipulation.hpp"
 
 namespace fs = std::filesystem;
-
-void print_file(const fs::path& filepath) {
-    std::ifstream file(filepath);
-    if (!file.is_open()) {
-        std::cerr << "Error: Could not open file " << filepath << std::endl;
-        return;
-    }
-
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
-    }
-    file.close();
-}
 
 int main(int argc, char* argv[]) {
     CommandLineParser parser;
     parser.parse(argc, argv);
 
     auto input_path = parser.getInputPath();
-    print_file(input_path);
+
+    FileOutput fileOutput;
+    fileOutput.printFile(input_path);
+
+    // Example usage of FileManipulation
+    FileManipulation fileManip;
+    std::vector<std::string> lines = fileManip.readLines(input_path);
+    fileManip.writeLines("output.txt", lines);
+    fileManip.appendLine("output.txt", "This is a new line");
 
     return 0;
 }
